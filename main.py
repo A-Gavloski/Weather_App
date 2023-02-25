963b7a03b072c95e5e585a15acbc6ed89160d1a7
Andrew <andrew-gavloski@hotmail.com>
2023-02-24 22:12:04 -0500

    New commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import datetime as dt
+import requests
+
+
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+API_KEY = "8e5e4bbcd050cb6824f7d32cf3e54bcf"
+CITY = "Toronto"
+
+
+def Kelvin_to_celsius(kelvin):
+    celsius = kelvin - 273.15
+    return celsius
+
+
+url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+response = requests.get(url).json()
+
+
+temp_celsius = response['main']['temp']
+temp_celsius = Kelvin_to_celsius(temp_celsius)
+feels_like_celsius = response['main']['feels_like']
+feels_like_celsius = Kelvin_to_celsius(feels_like_celsius)
+humidity = response['main']['humidity']
+wind_speed = response['wind']['speed']
+
+
+print(f"Temperature in {CITY}: {temp_celsius:.2f}°C")
+print(f"Temperature in {CITY} feels like: {feels_like_celsius:.2f}°C")
+print(f"Humidity in {CITY}: {humidity:}%")
+print(f"Wind speed in {CITY}: {wind_speed:.2f}°C")
